b27be559c472ae2f56f8641541137c2b6299d34b
Henri Vandersleyen <henri-vandersleyen@protonmail.com>
2022-11-13 11:07:40 -0800

    Added global vars

---
+++ frontend/src/global.d.ts
@@ -0,0 +1,10 @@
+interface ImportMetaEnv {
+  VITE_WEBSITE_STATE: string;
+  VITE_GO_BACKEND_URL: string;
+  VITE_GO_BACKEND_PORT: string;
+  VITE_SITE_URL: string;
+  VITE_SITE_PORT: string;
+  VITE_CONTACT_EMAIL: string;
+  VITE_GITHUB_PAGE: string;
+}
+
